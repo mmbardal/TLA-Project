bf46e6effb89edf5b6c041334e2599f87d02f0df
Koroush Taghi Pour <koroush.t81@gmail.com>
2023-05-07 14:28:09 +0330

    CheckString method got seperated.

---
+++ Phase3/CheckString.py
@@ -0,0 +1,19 @@
+import OriginalData
+
+def CheckString(stri,num,state):
+    
+    if(num==len(stri) and state in OriginalData.final_states):
+        return "Accepted"
+    elif(num==len(stri) and state not in OriginalData.final_states):
+        return "Rejected"
+    
+    currentAlphabet=stri[num]
+    if(currentAlphabet not in OriginalData.originalTransitions[state]):
+        return "Rejected"
+    else:
+        for sattes in OriginalData.originalTransitions[state][currentAlphabet]:
+            ans=CheckString(stri,num+1,sattes)
+            if(ans=="Accepted"):
+                return "Accepted"
+            
+        return "Rejected"
